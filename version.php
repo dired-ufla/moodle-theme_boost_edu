7780ce8899b8018bd3d8366f7ab50622787dbfde
Paulo Júnior <pauloa.junior@ufla.br>
2025-12-18 22:52:27 -0300

    fix version attributes

--- version.php
+++ version.php
@@ -25,17 +25,21 @@
 // This line protects the file from being accessed by a URL directly.                                                               
 defined('MOODLE_INTERNAL') || die();                                                                                                
                                                                                                                                     
-// This is the version of the plugin.                                                                                               
-$plugin->version = '2025121200';                                                                                                    
+// This is the version of the plugin.                                                                                                
+$plugin->version = 2025121200;                                                                                                    
                                                                                                                                     
 // This is the version of Moodle this plugin requires.                                                                              
-$plugin->requires = '2016070700';                                                                                                   
+$plugin->requires = 2016070700;                                                                                                   
                                                                                                                                     
 // This is the component name of the plugin - it always starts with 'theme_'                                                        
 // for themes and should be the same as the name of the folder.                                                                     
-$plugin->component = 'theme_boost_edu';                                                                                                 
+$plugin->component = 'theme_boost_edu';                      
+                                                                           
+// Maturity and human-readable release label.                                                                                         
+$plugin->maturity = MATURITY_BETA;                                                                                                    
+$plugin->release  = '1.0.0-beta (Build: 2025121200)';                                                                                 
                                                                                                                                     
 // This is a list of plugins, this plugin depends on (and their versions).                                                          
 $plugin->dependencies = [                                                                                                           
-    'theme_boost' => '2016102100'                                                                                                   
+    'theme_boost' => 2016102100                                                                                                   
 ];
